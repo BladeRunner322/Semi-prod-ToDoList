e241c46b19514cb5c9cb029b8d0b3847a0623d5c
BladeRunner322 <tigrcatwot@gmail.com>
2026-07-13 21:40:24 +0300

    add frontend (index.html fully vibecoded)

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -22,6 +22,9 @@ import (
 	users_postgres_repository "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/users/repository/postgres"
 	users_service "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/users/service"
 	users_transport_http "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/users/transport/http"
+	web_fs_repository "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/web/repository/file_system"
+	web_service "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/web/service"
+	web_transport_http "github.com/BladeRunner322/Semi-prod-ToDoList/internal/features/web/transport/http"
 	"go.uber.org/zap"
 
 	_ "github.com/BladeRunner322/Semi-prod-ToDoList/docs"
@@ -78,6 +81,11 @@ func main() {
 	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
 	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
 
+	logger.Debug("initialising feature", zap.String("feature", "web"))
+	webRepository := web_fs_repository.NewWebRepository()
+	webService := web_service.NewWebService(webRepository)
+	webTransportHTTP := web_transport_http.NewWebHTTPHandler(webService)
+
 	logger.Debug("initialising HTTP server")
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -104,6 +112,8 @@ func main() {
 		apiVersionRouterV1,
 		// apiVersionRouterV2,
 	)
+
+	httpServer.RegisterRoutes(webTransportHTTP.Routes()...)
 
 	httpServer.RegisterSwagger()
 
